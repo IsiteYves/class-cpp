c3acd3c150671b0b4c4778dc6a755367d5280282
IsiteYves <yvesisite@gmail.com>
2022-05-09 11:48:25 +0200

    [FEAT] Added more functionalities (add edge, remove edge)

--- Adjacency-matrix.cpp
+++ Adjacency-matrix.cpp
@@ -28,10 +28,33 @@ public:
             cout << endl;
         }
     }
+    void add_edge(int origin, int destination)
+    {
+        if (origin < 0 || destination < 0 || origin > nodes || destination > nodes)
+        {
+            cout << "Invalid node" << endl;
+            return;
+        }
+        matrix[origin][destination] = 1;
+    }
+    void remove_edge(int origin, int destination)
+    {
+        if (origin < 0 || destination < 0 || origin > nodes || destination > nodes)
+        {
+            cout << "Invalid node" << endl;
+            return;
+        }
+        matrix[origin][destination] = 0;
+    }
 };
 
 int main()
 {
     AdjacencyMatrix adj(5);
+    adj.display();
+    adj.add_edge(2, 3);
+    adj.add_edge(3, 3);
+    adj.remove_edge(2, 3);
+    cout << "\n";
     adj.display();
 }
